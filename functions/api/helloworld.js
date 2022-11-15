9b0ca487cd06d05ee4a2d4e7f5bf933859e8d07a
winterreisender <winterreisender@foxmail.com>
2022-11-15 12:29:41 +0800

    update

--- functions/api/helloworld.js
+++ functions/api/helloworld.js
@@ -2,10 +2,10 @@ export default {
   async fetch(request, env) {
     let counter = parseInt(env.KVStore.get("counter"))
     env.KVStore.put("counter",counter+1);
-    return await handleRequest(request)
+    return await handleRequest(request,counter)
   }
 }
 
-async function handleRequest(request) {
-  return new Response("Hello world")
+async function handleRequest(request,counter) {
+  return new Response(`Hello world ${counter}`)
 }
