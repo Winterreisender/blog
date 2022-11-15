d4ce9a49c30dbd499f9c7c3ef30bb4e37cf9b04f
winterreisender <winterreisender@foxmail.com>
2022-11-15 12:28:24 +0800

    update

--- functions/api/helloworld.js
+++ functions/api/helloworld.js
@@ -1,20 +1,11 @@
-export async function onRequest(context) {
-    // Contents of context object
-    const {
-      request, // same as existing Worker API
-      env, // same as existing Worker API
-      params, // if filename includes [id] or [[path]]
-      waitUntil, // same as ctx.waitUntil in existing Worker API
-      passThroughOnException, // same as ctx.passThroughOnException in existing Worker API
-      next, // used for middleware or to fetch assets
-      data, // arbitrary space for passing data between middlewares
-    } = context;
-  
-    
-    try {
-      KVSTORE.put("OK",1);
-    } catch(e) {
-      
-    }
-    return new Response(`Hello, world! ${request.url}`);
+export default {
+  async fetch(request, env) {
+    let counter = parseInt(env.KVStore.get("counter"))
+    env.KVStore.put("counter",counter+1);
+    return await handleRequest(request)
   }
+}
+
+async function handleRequest(request) {
+  return new Response("Hello world")
+}
